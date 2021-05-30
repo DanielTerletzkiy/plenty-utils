34ff558a61e92f1350e56990314e22d67a8bae42
danielterletzkiy <danielterletzkiy@gmail.com>
2021-05-30 04:26:49 +0200

    history-api-fallback

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
 const path = require("path");
 const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true,
+  })
+);
 const port = process.env.PORT || 80;
 app.listen(port);
