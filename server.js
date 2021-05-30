00a7b8b64c05c021e59ae271c9a5f7ef7e79a2f9
danielterletzkiy <danielterletzkiy@gmail.com>
2021-05-30 04:31:36 +0200

    .

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const history = require("connect-history-api-fallback");
-const path = require("path");
+const express = require('express');
+const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+const path = require('path');
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+app.use(staticFileMiddleware);
 app.use(
   history({
     disableDotRule: true,
     verbose: true,
   })
 );
+app.use(staticFileMiddleware);
 const port = process.env.PORT || 80;
 app.listen(port);
