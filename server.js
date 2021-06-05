1a6145f1342a41ca9fb55de442a768edddcbd246
danielterletzkiy <danielterletzkiy@gmail.com>
2021-06-05 04:58:15 +0200

    fix?

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const history = require('connect-history-api-fallback');
 const path = require('path');
 const app = express();
 
-var pcpartpicker = require('./src/components/Computer/components/helper/pcpartpicker');
-const puppeteer = require('puppeteer');
+//var pcpartpicker = require('./src/components/Computer/components/helper/pcpartpicker');
+//const puppeteer = require('puppeteer');
 
 var port = process.env.PORT || 3080;
 app.listen(port, function () {
@@ -14,31 +14,31 @@ app.listen(port, function () {
 
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.get('/api/test', function (req, res) {
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto('https://geizhals.de/?cat=cpuamdam4', {
-      waitUntil: 'networkidle2',
-    });
-
-    //console.log(page);
-    //await new Promise((resolve) => setTimeout(resolve, 10000));
-    var innerText=
-      await page.evaluate(
-        () =>
-          document.getElementsByClassName(
-            'filtercategory__productlist-scroll-wrapper'
-          )[0].innerText
-    );
-
-    res.send(innerText);
-
-
-    await browser.close();
-  })();
-});
+//app.get('/api/test', function (req, res) {
+//  (async () => {
+//    const browser = await puppeteer.launch();
+//    const page = await browser.newPage();
+//
+//    await page.goto('https://geizhals.de/?cat=cpuamdam4', {
+//      waitUntil: 'networkidle2',
+//    });
+//
+//    //console.log(page);
+//    //await new Promise((resolve) => setTimeout(resolve, 10000));
+//    var innerText=
+//      await page.evaluate(
+//        () =>
+//          document.getElementsByClassName(
+//            'filtercategory__productlist-scroll-wrapper'
+//          )[0].innerText
+//    );
+//
+//    res.send(innerText);
+//
+//
+//    await browser.close();
+//  })();
+//});
 
 app.use(express.json({ limit: '16mb' }));
 app.use(
